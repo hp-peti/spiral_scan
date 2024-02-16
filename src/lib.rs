7b00d609673d632c829771c499c5e5de78b1c1a3
Hanos-Puskai Peter <hp.peti@gmail.com>
2024-02-16 15:11:48 +0200

    added options for spiral ordering and reversals

--- src/lib.rs
+++ src/lib.rs
@@ -1,78 +1,326 @@
 use numpy::ndarray::Array2;
 use numpy::{IntoPyArray, Ix2, PyArray2};
 
-use pyo3::{pymodule, types::PyModule, PyResult, Python};
+use pyo3::pyclass;
+use pyo3::{pymethods, pymodule, types::PyModule, PyResult, Python};
 
 #[pymodule]
 fn spiral_scan<'py>(_py: Python<'py>, m: &'py PyModule) -> PyResult<()> {
+    m.add_class::<SpiralOrdering>()?;
+
     #[pyfn(m)]
-    //#[pyo3(name = "generate_2d_matrix")]
-    fn generate_2d_matrix<'py>(py: Python<'py>, n: usize, m: usize) -> &'py PyArray2<i64> {
+    #[pyo3(name = "generate_2d_matrix", signature=(n, m, ordering=SpiralOrdering::TopLeftCW, reversed=false))]
+    fn generate_2d_matrix<'py>(
+        py: Python<'py>,
+        n: usize,
+        m: usize,
+        ordering: SpiralOrdering,
+        reversed: bool,
+    ) -> PyResult<&'py PyArray2<i64>> {
         let mut result = Array2::<i64>::zeros(Ix2(n, m));
-        let mut k = 0;
 
-        spiral(n, m, |i: usize, j: usize| {
-            result[[i, j]] = k;
-            k += 1;
-        });
+        if !reversed {
+            let mut k = 0i64;
+            spiral(n, m, ordering, |i: usize, j: usize| {
+                result[[i, j]] = k;
+                k += 1;
+            });
+        } else {
+            let mut k = i64::try_from(n * m)?;
+            spiral(n, m, ordering, |i: usize, j: usize| {
+                k -= 1;
+                result[[i, j]] = k;
+            });
+        }
 
-        result.into_pyarray(py)
+        Ok(result.into_pyarray(py))
     }
 
     #[pyfn(m)]
-    //#[pyo3(name = "generate_2d_indices")]
-    fn generate_2d_indices<'py>(py: Python<'py>, n: usize, m: usize) -> &'py PyArray2<usize> {
+    #[pyo3(name = "generate_2d_indices", signature=(n, m, ordering=SpiralOrdering::TopLeftCW, reversed=false))]
+    fn generate_2d_indices<'py>(
+        py: Python<'py>,
+        n: usize,
+        m: usize,
+        ordering: SpiralOrdering,
+        reversed: bool,
+    ) -> &'py PyArray2<usize> {
         let mut result = Array2::<usize>::zeros(Ix2(n * m, 2));
-        let mut k = 0usize;
-        spiral(n, m, |i: usize, j: usize| {
-            result[[k, 0]] = i;
-            result[[k, 1]] = j;
-            k += 1;
-        });
-
+        if !reversed {
+            let mut k = 0usize;
+            spiral(n, m, ordering, |i: usize, j: usize| {
+                result[[k, 0]] = i;
+                result[[k, 1]] = j;
+                k += 1;
+            });
+        } else {
+            let mut k = n * m;
+            spiral(n, m, ordering, |i: usize, j: usize| {
+                k -= 1;
+                result[[k, 0]] = i;
+                result[[k, 1]] = j;
+            });
+        }
         result.into_pyarray(py)
     }
 
     Ok(())
 }
 
-/// Template function to generate spiral scan coordonates
-fn spiral<F: FnMut(usize, usize)>(n: usize, m: usize, mut collect: F) {
-    let (mut start_i, mut stop_i) = (0usize, n);
-    let (mut start_j, mut stop_j) = (0usize, m);
+pub struct SpiralScan<Collect: FnMut(usize, usize)> {
+    start_i: usize,
+    stop_i: usize,
+    start_j: usize,
+    stop_j: usize,
+    collect: Collect,
+}
 
-    while start_i < stop_i || start_j < stop_j {
+impl<Collect: FnMut(usize, usize)> SpiralScan<Collect> {
+    pub fn new(n: usize, m: usize, collect: Collect) -> SpiralScan<Collect> {
+        SpiralScan {
+            start_i: 0,
+            stop_i: n,
+            start_j: 0,
+            stop_j: m,
+            collect,
+        }
+    }
+    pub fn has_more(&self) -> bool {
+        self.start_i < self.stop_i || self.start_j < self.stop_j
+    }
+
+    pub fn go_top_right(&mut self) {
         // →→→
-        if start_i < stop_i {
-            let i = start_i;
-            for j in start_j..stop_j {
-                collect(i, j)
+        if self.start_i < self.stop_i {
+            let i = self.start_i;
+            for j in self.start_j..self.stop_j {
+                (self.collect)(i, j)
             }
-            start_i += 1;
+            self.start_i += 1;
         }
+    }
+
+    pub fn go_rightside_down(&mut self) {
         // ↓↓↓
-        if start_j < stop_j {
-            let j = stop_j - 1;
-            for i in start_i..stop_i {
-                collect(i, j);
+        if self.start_j < self.stop_j {
+            let j = self.stop_j - 1;
+            for i in self.start_i..self.stop_i {
+                (self.collect)(i, j);
             }
-            stop_j -= 1;
+            self.stop_j -= 1;
         }
+    }
+
+    pub fn go_bottom_left(&mut self) {
         // ←←←
-        if start_i < stop_i {
-            let i = stop_i - 1;
-            for j in start_j..stop_j {
-                collect(i, j);
+        if self.start_i < self.stop_i {
+            let i = self.stop_i - 1;
+            for j in (self.start_j..self.stop_j).rev() {
+                (self.collect)(i, j);
             }
-            stop_i -= 1;
+            self.stop_i -= 1;
         }
+    }
+    pub fn go_leftside_up(&mut self) {
         // ↑↑↑
-        if start_j < stop_j {
-            let j = start_j;
-            for i in (start_i..stop_i).rev() {
-                collect(i, j);
+        if self.start_j < self.stop_j {
+            let j = self.start_j;
+            for i in (self.start_i..self.stop_i).rev() {
+                (self.collect)(i, j);
             }
-            start_j += 1;
+            self.start_j += 1;
+        }
+    }
+
+    // -----
+
+    pub fn go_top_left(&mut self) {
+        // ←←←
+        if self.start_i < self.stop_i {
+            let i = self.start_i;
+            for j in (self.start_j..self.stop_j).rev() {
+                (self.collect)(i, j)
+            }
+            self.start_i += 1;
+        }
+    }
+
+    pub fn go_rightside_up(&mut self) {
+        // ↑↑↑
+        if self.start_j < self.stop_j {
+            let j = self.stop_j - 1;
+            for i in (self.start_i..self.stop_i).rev() {
+                (self.collect)(i, j);
+            }
+            self.stop_j -= 1;
+        }
+    }
+
+    pub fn go_bottom_right(&mut self) {
+        // →→→
+        if self.start_i < self.stop_i {
+            let i = self.stop_i - 1;
+            for j in self.start_j..self.stop_j {
+                (self.collect)(i, j);
+            }
+            self.stop_i -= 1;
+        }
+    }
+
+    pub fn go_leftside_down(&mut self) {
+        // ↓↓↓
+        if self.start_j < self.stop_j {
+            let j = self.start_j;
+            for i in self.start_i..self.stop_i {
+                (self.collect)(i, j);
+            }
+            self.start_j += 1;
+        }
+    }
+}
+
+#[pyclass]
+#[derive(Clone, Copy)]
+enum SpiralOrdering {
+    TopLeftCW,
+    TopRightCW,
+    BottomRightCW,
+    BottomLeftCW,
+    TopLeftCCW,
+    BottomLeftCCW,
+    BottomRightCCW,
+    TopRightCCW,
+}
+
+#[pymethods]
+#[rustfmt::skip]
+impl SpiralOrdering {
+    fn identity(&self) -> SpiralOrdering {
+        match self {
+            SpiralOrdering::TopLeftCW       => SpiralOrdering::TopLeftCW     ,
+            SpiralOrdering::TopRightCW      => SpiralOrdering::TopRightCW    ,
+            SpiralOrdering::BottomRightCW   => SpiralOrdering::BottomRightCW ,
+            SpiralOrdering::BottomLeftCW    => SpiralOrdering::BottomLeftCW  ,
+            SpiralOrdering::TopLeftCCW      => SpiralOrdering::TopLeftCCW    ,
+            SpiralOrdering::BottomLeftCCW   => SpiralOrdering::BottomLeftCCW ,
+            SpiralOrdering::BottomRightCCW  => SpiralOrdering::BottomRightCCW,
+            SpiralOrdering::TopRightCCW     => SpiralOrdering::TopRightCCW   ,
+        }
+    }
+    fn flip_rot(&self) -> SpiralOrdering {
+        match self {
+            SpiralOrdering::TopLeftCW       => SpiralOrdering::TopLeftCCW     ,
+            SpiralOrdering::TopRightCW      => SpiralOrdering::TopRightCCW    ,
+            SpiralOrdering::BottomRightCW   => SpiralOrdering::BottomRightCCW ,
+            SpiralOrdering::BottomLeftCW    => SpiralOrdering::BottomLeftCCW  ,
+            SpiralOrdering::TopLeftCCW      => SpiralOrdering::TopLeftCW    ,
+            SpiralOrdering::BottomLeftCCW   => SpiralOrdering::BottomLeftCW ,
+            SpiralOrdering::BottomRightCCW  => SpiralOrdering::BottomRightCW,
+            SpiralOrdering::TopRightCCW     => SpiralOrdering::TopRightCW   ,
+        }
+    }
+    fn rotate_cw(&self) -> SpiralOrdering {
+        match self {
+            SpiralOrdering::TopLeftCW       => SpiralOrdering::TopRightCW    ,
+            SpiralOrdering::TopRightCW      => SpiralOrdering::BottomRightCW ,
+            SpiralOrdering::BottomRightCW   => SpiralOrdering::BottomLeftCW  ,
+            SpiralOrdering::BottomLeftCW    => SpiralOrdering::TopLeftCW     ,
+            SpiralOrdering::TopLeftCCW      => SpiralOrdering::TopRightCCW   ,
+            SpiralOrdering::BottomLeftCCW   => SpiralOrdering::TopLeftCCW    ,
+            SpiralOrdering::BottomRightCCW  => SpiralOrdering::BottomLeftCCW ,
+            SpiralOrdering::TopRightCCW     => SpiralOrdering::BottomRightCCW,
+        }
+    }
+    fn rotate_ccw(&self) -> SpiralOrdering {
+        match self {
+            SpiralOrdering::TopLeftCW       => SpiralOrdering::BottomLeftCW  ,
+            SpiralOrdering::TopRightCW      => SpiralOrdering::TopLeftCW     ,
+            SpiralOrdering::BottomRightCW   => SpiralOrdering::TopRightCW    ,
+            SpiralOrdering::BottomLeftCW    => SpiralOrdering::BottomRightCW ,
+            SpiralOrdering::TopLeftCCW      => SpiralOrdering::BottomLeftCCW ,
+            SpiralOrdering::BottomLeftCCW   => SpiralOrdering::BottomRightCCW,
+            SpiralOrdering::BottomRightCCW  => SpiralOrdering::TopRightCCW   ,
+            SpiralOrdering::TopRightCCW     => SpiralOrdering::TopLeftCCW    ,
+        }
+    }
+    fn rotate_180(&self) -> SpiralOrdering {
+        return self.rotate_cw().rotate_cw();
+    }
+
+    fn reverse(&self) -> SpiralOrdering {
+        return self.rotate_180().flip_rot()
+    }
+
+}
+
+/// Template function to generate spiral scan coordonates
+fn spiral<F: FnMut(usize, usize)>(n: usize, m: usize, ordering: SpiralOrdering, collect: F) {
+    let mut scan = SpiralScan::new(n, m, collect);
+
+    match ordering {
+        SpiralOrdering::TopLeftCW => {
+            while scan.has_more() {
+                scan.go_top_right();
+                scan.go_rightside_down();
+                scan.go_bottom_left();
+                scan.go_leftside_up();
+            }
+        }
+        SpiralOrdering::TopRightCW => {
+            while scan.has_more() {
+                scan.go_rightside_down();
+                scan.go_bottom_left();
+                scan.go_leftside_up();
+                scan.go_top_right();
+            }
+        }
+        SpiralOrdering::BottomRightCW => {
+            while scan.has_more() {
+                scan.go_bottom_left();
+                scan.go_leftside_up();
+                scan.go_top_right();
+                scan.go_rightside_down();
+            }
+        }
+        SpiralOrdering::BottomLeftCW => {
+            while scan.has_more() {
+                scan.go_leftside_up();
+                scan.go_top_right();
+                scan.go_rightside_down();
+                scan.go_bottom_left();
+            }
+        }
+        SpiralOrdering::TopLeftCCW => {
+            while scan.has_more() {
+                scan.go_leftside_down();
+                scan.go_bottom_right();
+                scan.go_rightside_up();
+                scan.go_top_left();
+            }
+        }
+        SpiralOrdering::BottomLeftCCW => {
+            while scan.has_more() {
+                scan.go_bottom_right();
+                scan.go_rightside_up();
+                scan.go_top_left();
+                scan.go_leftside_down();
+            }
+        }
+        SpiralOrdering::BottomRightCCW => {
+            while scan.has_more() {
+                scan.go_rightside_up();
+                scan.go_top_left();
+                scan.go_leftside_down();
+                scan.go_bottom_right();
+            }
+        }
+        SpiralOrdering::TopRightCCW => {
+            while scan.has_more() {
+                scan.go_top_left();
+                scan.go_leftside_down();
+                scan.go_bottom_right();
+                scan.go_rightside_up();
+            }
         }
     }
 }
